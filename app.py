9dc4e62d8f38be9201807b39cd545e017ca1023e
dongcheol <dongcheol@comcom.ai>
2020-05-12 10:07:10 +0900

    Indentation

--- app.py
+++ app.py
@@ -14,34 +14,34 @@ import tensorflow_hub as hub
 hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 
 def load_img(img):
-  byteIO = io.BytesIO()
-  img.save(byteIO, format='PNG')
-  byteArr = byteIO.getvalue()
+    byteIO = io.BytesIO()
+    img.save(byteIO, format='PNG')
+    byteArr = byteIO.getvalue()
 
-  img = tf.image.decode_image(byteArr, channels=3)
-  img = tf.image.convert_image_dtype(img, tf.float32)
+    img = tf.image.decode_image(byteArr, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
 
-  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
-  max_dim = 512
-  long_dim = max(shape)
-  scale = max_dim / long_dim
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    max_dim = 512
+    long_dim = max(shape)
+    scale = max_dim / long_dim
 
-  new_shape = tf.cast(shape * scale, tf.int32)
+    new_shape = tf.cast(shape * scale, tf.int32)
 
-  img = tf.image.resize(img, new_shape)
-  img = img[tf.newaxis, :]
-  return img
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
 
 def tensor_to_image(tensor):
-  tensor = tensor*255
-  tensor = np.array(tensor, dtype=np.uint8)
+    tensor = tensor*255
+    tensor = np.array(tensor, dtype=np.uint8)
 
-  if np.ndim(tensor)>3:
-    assert tensor.shape[0] == 1
-    tensor = tensor[0]
-  return PIL.Image.fromarray(tensor)
+    if np.ndim(tensor) > 3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return PIL.Image.fromarray(tensor)
 
-app = Flask(__name__) 
+app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 8
 
 limiter = Limiter(
@@ -49,29 +49,30 @@ limiter = Limiter(
     default_limits=['1 per second']
 )
 
+
 @app.route('/transfer', methods=['POST'])
 def transfer():
-    ## check isImage
-    
+    # check isImage
     if not request.files.get('baseImage'):
-        return { 'error': 'must have a baseImage' }, 400
+        return {'error': 'must have a baseImage'}, 400
     if not request.files.get('styleImage'):
-        return { 'error': 'must have a styleImage' }, 400
+        return {'error': 'must have a styleImage'}, 400
 
     try:
         baseImage = Image.open(request.files['baseImage'].stream)
         styleImage = Image.open(request.files['styleImage'].stream)
 
         if(baseImage.format not in ['JPG', 'JPEG', 'PNG']):
-            return { 'error': 'image must be jpg, jpeg or png' }, 400
+            return {'error': 'image must be jpg, jpeg or png'}, 400
 
         if(styleImage.format not in ['JPG', 'JPEG', 'PNG']):
-            return { 'error': 'image must be jpg, jpeg or png' }, 400
+            return {'error': 'image must be jpg, jpeg or png'}, 400
 
         baseImage = load_img(baseImage)
         styleImage = load_img(styleImage)
 
-        stylized_image = hub_module(tf.constant(baseImage), tf.constant(styleImage))[0]
+        stylized_image = hub_module(tf.constant(
+            baseImage), tf.constant(styleImage))[0]
 
         output = tensor_to_image(stylized_image)
 
@@ -80,8 +81,8 @@ def transfer():
         img_io.seek(0)
         return send_file(img_io, mimetype='image/png')
 
-    except Exception :
-        return { 'error': 'can not load your image files. check your image files' }, 400
+    except Exception:
+        return {'error': 'can not load your image files. check your image files'}, 400
 
 @app.route('/healthz')
 def health():
@@ -89,7 +90,7 @@ def health():
 
 @app.errorhandler(413)
 def request_entity_too_large(error):
-    return { 'error': 'File Too Large' }, 413
+    return {'error': 'File Too Large'}, 413
 
 if __name__ == '__main__':
     app.run(debug=False, port=80, host='0.0.0.0')
