3c7ade602607e7316642645829ce823746eb221d
dongcheol <dongcheol@comcom.ai>
2020-05-12 11:08:28 +0900

    change indentation 4 to 2

--- app.py
+++ app.py
@@ -12,46 +12,45 @@ app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 8
 
 limiter = Limiter(
-    app,
-    default_limits=['1 per second']
+  app,
+  default_limits=['1 per second']
 )
 
 @app.route('/transfer', methods=['POST'])
 def transfer():
-    if not request.files.get('baseImage'):
-        return {'error': 'must have a baseImage'}, 400
-    if not request.files.get('styleImage'):
-        return {'error': 'must have a styleImage'}, 400
+  if not request.files.get('baseImage'):
+    return {'error': 'must have a baseImage'}, 400
+  if not request.files.get('styleImage'):
+    return {'error': 'must have a styleImage'}, 400
 
-    try:
-        baseImage = Image.open(request.files['baseImage'].stream)
-        styleImage = Image.open(request.files['styleImage'].stream)
+  try:
+    baseImage = Image.open(request.files['baseImage'].stream)
+    styleImage = Image.open(request.files['styleImage'].stream)
 
-        if(baseImage.format not in ['JPG', 'JPEG', 'PNG']):
-            return {'error': 'image must be jpg, jpeg or png'}, 400
+    if(baseImage.format not in ['JPG', 'JPEG', 'PNG']):
+      return {'error': 'image must be jpg, jpeg or png'}, 400
 
-        if(styleImage.format not in ['JPG', 'JPEG', 'PNG']):
-            return {'error': 'image must be jpg, jpeg or png'}, 400
+    if(styleImage.format not in ['JPG', 'JPEG', 'PNG']):
+      return {'error': 'image must be jpg, jpeg or png'}, 400
 
-        baseImage = load_img(baseImage)
-        styleImage = load_img(styleImage)
+    baseImage = load_img(baseImage)
+    styleImage = load_img(styleImage)
 
-        stylized_image = hub_module(tf.constant(
-            baseImage), tf.constant(styleImage))[0]
+    stylized_image = hub_module(tf.constant(baseImage), tf.constant(styleImage))[0]
 
-        output = tensor_to_image(stylized_image)
+    output = tensor_to_image(stylized_image)
 
-        return send_file(output, mimetype='image/png')
-    except Exception:
-        return {'error': 'can not load your image files. check your image files'}, 400
+    return send_file(output, mimetype='image/png')
+  except Exception:
+    return {'error': 'can not load your image files. check your image files'}, 400
 
 @app.route('/healthz')
 def health():
-    return "ok"
+  return "ok"
 
 @app.errorhandler(413)
 def request_entity_too_large(error):
-    return {'error': 'File Too Large'}, 413
+  return {'error': 'File Too Large'}, 413
 
 if __name__ == '__main__':
-    app.run(debug=False, port=80, host='0.0.0.0')
+  app.run(debug=False, port=80, host='0.0.0.0')
--- imageLoader.py
+++ imageLoader.py
@@ -9,35 +9,35 @@ import PIL.Image
 import io
 
 def load_img(img):
-    byteIO = io.BytesIO()
-    img.save(byteIO, format='PNG')
-    byteArr = byteIO.getvalue()
+  byteIO = io.BytesIO()
+  img.save(byteIO, format='PNG')
+  byteArr = byteIO.getvalue()
 
-    img = tf.image.decode_image(byteArr, channels=3)
-    img = tf.image.convert_image_dtype(img, tf.float32)
+  img = tf.image.decode_image(byteArr, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
 
-    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
-    max_dim = 512
-    long_dim = max(shape)
-    scale = max_dim / long_dim
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  max_dim = 512
+  long_dim = max(shape)
+  scale = max_dim / long_dim
 
-    new_shape = tf.cast(shape * scale, tf.int32)
+  new_shape = tf.cast(shape * scale, tf.int32)
 
-    img = tf.image.resize(img, new_shape)
-    img = img[tf.newaxis, :]
-    return img
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
 
 def tensor_to_image(tensor):
-    tensor = tensor * 255
-    tensor = np.array(tensor, dtype=np.uint8)
+  tensor = tensor * 255
+  tensor = np.array(tensor, dtype=np.uint8)
 
-    if np.ndim(tensor) > 3:
-        assert tensor.shape[0] == 1
-        tensor = tensor[0]
-    
-    output = PIL.Image.fromarray(tensor)
-    img_io = io.BytesIO()
-    output.save(img_io, 'PNG', quality=70)
-    img_io.seek(0)
-    
-    return img_io
+  if np.ndim(tensor) > 3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  
+  output = PIL.Image.fromarray(tensor)
+  img_io = io.BytesIO()
+  output.save(img_io, 'PNG', quality=70)
+  img_io.seek(0)
+  
+  return img_io
